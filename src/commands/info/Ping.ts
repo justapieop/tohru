cc6409f048189e3899395e918964fc83b97879cb
JustAPie <justapie06@gmail.com>
2023-05-23 10:32:35 +0700

    feat: add command handling and ping command

---
+++ src/commands/info/Ping.ts
@@ -0,0 +1,30 @@
+import { Colors, CommandInteraction, Message } from "discord.js";
+import { Discord, Slash } from "discordx";
+
+@Discord()
+export class Ping {
+    @Slash({ name: "ping", description: "Show how long it takes for Tohru to fly from Kobayashi's home." })
+    public async ping(interaction: CommandInteraction): Promise<void> {
+        const wsPing: number = interaction.client.ws.ping;
+
+        const msg: Message = await interaction.editReply({
+            embeds: [
+                {
+                    color: Colors.Green,
+                    description: "🏓 Pinging..."
+                }
+            ]
+        });
+
+        const ping: number = Math.abs(Date.now() - msg.createdTimestamp);
+
+        await msg.edit({
+            embeds: [
+                {
+                    color: Colors.Green,
+                    description: `🏓 Ping: ${ping}ms\n🏓 Websocket Ping: ${wsPing}ms`
+                }
+            ]
+        });
+    }
+}
