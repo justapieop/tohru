73ca1783a93b6716da9cfc9f682023d45d22d764
JustAPie <justapie06@gmail.com>
2023-05-23 15:30:00 +0700

    feat: add mongodb support

--- src/launch.ts
+++ src/launch.ts
@@ -1,5 +1,12 @@
 (await import("dotenv")).config();
 import "reflect-metadata";
 import { Tohru } from "./client/Tohru.js";
+import { DBClient } from "./modules/db/DBClient.js";
+import { Logger } from "./utils/Logger.js";
 
 await new Tohru().run();
+DBClient.getClient().then(
+    () => Logger.getLogger().info("Connected to MongoDB instance.")
+).catch(
+    () => Logger.getLogger().info("Failed to connect to MongoDB instance.")
+);
