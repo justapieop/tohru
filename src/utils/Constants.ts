8e3489ada21c0ec0526244805255d152d29abca2
JustAPie <justapie06@gmail.com>
2023-05-23 10:32:12 +0700

    feat: add message content intent

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -5,7 +5,8 @@ export class Constants {
     public static readonly GATEWAY_INTENTS: GatewayIntentBits =
         IntentsBitField.Flags.Guilds |
         IntentsBitField.Flags.GuildMembers |
-        IntentsBitField.Flags.GuildVoiceStates;
+        IntentsBitField.Flags.GuildVoiceStates |
+        IntentsBitField.Flags.MessageContent;
 
     public static readonly NODE_ENV_DEV: boolean = process.env.NODE_ENV.toLowerCase() === "dev";
 }
