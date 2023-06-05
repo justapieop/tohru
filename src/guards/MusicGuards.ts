ba1688a80823675c7c11e2eb975768a887998470
JustAPie <justapie06@gmail.com>
2023-06-05 09:49:21 +0700

    feat: add guards for music commands and buttons

--- src/guards/MusicGuards.ts
+++ src/guards/MusicGuards.ts
@@ -1,4 +1,4 @@
-import { Colors, CommandInteraction } from "discord.js";
+import { ButtonInteraction, Colors, CommandInteraction } from "discord.js";
 import { Client, GuardFunction, Next } from "discordx";
 import { KazagumoPlayer } from "kazagumo";
 
@@ -16,6 +16,53 @@ export class MusicGuards {
                     {
                         color: Colors.Red,
                         description: "❌ Please put something in the queue first!"
+                    }
+                ]
+            });
+            return;
+        }
+
+        await next();
+    }
+
+    public static async RequireActivePlayer(
+        interaction: CommandInteraction | ButtonInteraction,
+        client: Client,
+        next: Next,
+        guardData: any
+    ): Promise<GuardFunction<CommandInteraction | ButtonInteraction>> {
+        const player: KazagumoPlayer = client.music.getPlayer(interaction.guildId);
+
+        if (!player || !player.queue.current) {
+            if (interaction instanceof CommandInteraction) await interaction.reply({
+                embeds: [
+                    {
+                        color: Colors.Red,
+                        description: "❌ Please play something first!"
+                    }
+                ]
+            });
+            return;
+        }
+
+        guardData.player = player;
+
+        await next();
+    }
+
+    public static async RequireAvailablePlayer(
+        interaction: CommandInteraction | ButtonInteraction,
+        client: Client,
+        next: Next
+    ): Promise<GuardFunction<CommandInteraction | ButtonInteraction>> {
+        const player: KazagumoPlayer = client.music.getPlayer(interaction.guildId);
+
+        if (!player) {
+            if (interaction instanceof CommandInteraction) await interaction.reply({
+                embeds: [
+                    {
+                        color: Colors.Red,
+                        description: "❌ Please play something first!"
                     }
                 ]
             });
