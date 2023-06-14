df6d64f4a1e69437c3581b2ec13a31ec199720b4
JustAPie <justapie06@gmail.com>
2023-06-14 10:33:00 +0700

    feat: add volume command

---
+++ src/commands/music/volume.ts
@@ -0,0 +1,34 @@
+import { ApplicationCommandOptionType, Colors, CommandInteraction } from "discord.js";
+import { Discord, Slash, Guard, SlashOption, Client } from "discordx";
+import { MusicGuards } from "../../guards/MusicGuards.js";
+import { KazagumoPlayer } from "kazagumo";
+
+@Discord()
+export class Volume {
+    @Slash({ name: "volume", description: "Sets the volume of the player." })
+    @Guard(MusicGuards.RequireActivePlayer)
+    public async onVolume(
+        @SlashOption({
+            name: "volume",
+            description: "Volume from 0 - 200.",
+            type: ApplicationCommandOptionType.Number,
+            required: true,
+            minValue: 0,
+            maxValue: 200
+        })
+        volume: number,
+        interaction: CommandInteraction,
+        _: Client,
+        guardData: { player: KazagumoPlayer }
+    ): Promise<void> {
+        guardData.player.setVolume(volume);
+        await interaction.reply({
+            embeds: [
+                {
+                    color: Colors.Red,
+                    description: `✅ Successfully set the volume to \`${volume}%\`.`
+                }
+            ]
+        });
+    }
+}
