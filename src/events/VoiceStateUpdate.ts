d84f55b11b9dfc2b04ecaf341de5efd5259e2c4a
JustAPie <justapie06@gmail.com>
2023-06-14 10:48:04 +0700

    feat: add alwaysOn handler

--- src/events/VoiceStateUpdate.ts
+++ src/events/VoiceStateUpdate.ts
@@ -1,6 +1,7 @@
 import { GuildMember, StageChannel, VoiceBasedChannel } from "discord.js";
 import { ArgsOf, Client, Discord, On } from "discordx";
 import { KazagumoPlayer } from "kazagumo";
+import { GuildSetting, GuildSettingSchema, getGuildSetting } from "../modules/db/schemas/GuildSettings.js";
 
 @Discord()
 export class VoiceStateUpdate {
@@ -8,6 +9,7 @@ export class VoiceStateUpdate {
     public async onVoiceStateUpdate([oldState, newState]: ArgsOf<"voiceStateUpdate">, client: Client): Promise<void> {
         const player: KazagumoPlayer = client.music.getPlayer(newState.guild.id);
         const self: GuildMember = await (oldState.guild ?? newState.guild).members.fetchMe();
+        const guildSetting: GuildSettingSchema = await getGuildSetting((oldState ?? newState).guild.id);
         if (newState.channelId) {
             if (!oldState.channelId) {
                 const channel: VoiceBasedChannel = newState.channel;
@@ -23,10 +25,18 @@ export class VoiceStateUpdate {
         if (oldState.channelId && !newState.channelId) {
             if (oldState.channel.members.hasAny(self.id)) {
                 if (oldState.channel.members.filter((m: GuildMember) => !m.user.bot)) {
+                    if (guildSetting.alwaysOn) return;
                     if (player) player.destroy();
                     await self.voice.disconnect();
                 }
             } else if (oldState.member === self) {
+                await GuildSetting.findOneAndUpdate({
+                    id: (oldState ?? newState).guild.id
+                }, {
+                    $set: {
+                        alwaysOn: false
+                    }
+                });
                 if (player) player.destroy();
                 await self.voice.disconnect();
             }
