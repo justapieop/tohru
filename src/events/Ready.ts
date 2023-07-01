92cfa423c39581d5d8132c180ee38aaedd7491df
JustAPie <justapie06@gmail.com>
2023-07-01 10:56:58 +0700

    chore: add online timer update

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -1,12 +1,13 @@
+import prettyMs from "pretty-ms";
 import { Client, Discord, On } from "discordx";
 import { Logger } from "../utils/Logger.js";
 import { Constants } from "../utils/Constants.js";
+import { ActivityType } from "discord.js";
 
 @Discord()
 export class Ready {
     @On({ event: "ready" })
     public async onReady([], client: Client): Promise<void> {
-
         await client.clearApplicationCommands();
         if (Constants.NODE_ENV_DEV) await client.initApplicationCommands({
             global: {
@@ -18,6 +19,11 @@ export class Ready {
             }
         });
         else await client.initApplicationCommands();
+
+        setInterval(async () => {
+            const timeOnline: number = Date.now() - client.readyTimestamp;
+            client.user.setActivity(`Online for ${prettyMs(timeOnline)}`, { type: ActivityType.Streaming });
+        }, 5000);
 
         Logger.getLogger().info(`Logged in as ${client.user.tag}`);
     }
